334434b97ea2d282e998bdd3ea0f9f39c4430a42
Jack Sinko <jack@roadburngames.com>
2024-01-28 18:48:52 +1030

    Remove library folder after saving

--- src/save.ts
+++ src/save.ts
@@ -1,9 +1,22 @@
 import * as core from '@actions/core'
 import saveImpl from './saveImpl'
 import { StateProvider } from './stateProvider'
+import {Inputs} from "./constants";
 
 async function run(): Promise<void> {
   await saveImpl(new StateProvider())
+
+    try {
+        // Run the Linux command after saveImpl
+        const { stdout, stderr } = await execAsync(`rm -rf ${Inputs.Path}`);
+
+        // Handle the output if needed
+        console.log('Command Output:', stdout);
+        console.error('Command Error:', stderr);
+    } catch (error) {
+        // Handle errors if the command fails
+        console.error('Error running command:', error);
+    }
 }
 
 run()
